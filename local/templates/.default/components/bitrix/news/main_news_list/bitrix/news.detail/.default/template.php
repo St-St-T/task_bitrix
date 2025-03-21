43974aa5e8501bd8780fac0c31688131de3a78fd
Stanislav Stolyarov <stst09stanislav@yandex.ru>
2025-03-21 18:38:46 +0700

    [6] Применение вёрстки к детальному просмотру новости

--- local/templates/.default/components/bitrix/news/main_news_list/bitrix/news.detail/.default/template.php
+++ local/templates/.default/components/bitrix/news/main_news_list/bitrix/news.detail/.default/template.php
@@ -13,24 +13,28 @@
 $this->setFrameMode(true);
 ?>
 <div class="news-detail">
-	<?if($arParams["DISPLAY_PICTURE"]!="N" && is_array($arResult["DETAIL_PICTURE"])):?>
-		<img
-			class="detail_picture"
-			border="0"
-			src="<?=$arResult["DETAIL_PICTURE"]["SRC"]?>"
-			width="<?=$arResult["DETAIL_PICTURE"]["WIDTH"]?>"
-			height="<?=$arResult["DETAIL_PICTURE"]["HEIGHT"]?>"
-			alt="<?=$arResult["DETAIL_PICTURE"]["ALT"]?>"
-			title="<?=$arResult["DETAIL_PICTURE"]["TITLE"]?>"
-			/>
-	<?endif?>
-	<?if($arParams["DISPLAY_DATE"]!="N" && $arResult["DISPLAY_ACTIVE_FROM"]):?>
-		<span class="news-date-time"><?=$arResult["DISPLAY_ACTIVE_FROM"]?></span>
-	<?endif;?>
-	<?if($arParams["DISPLAY_NAME"]!="N" && $arResult["NAME"]):?>
-		<h3><?=$arResult["NAME"]?></h3>
-	<?endif;?>
-	<?if($arParams["DISPLAY_PREVIEW_TEXT"]!="N" && $arResult["FIELDS"]["PREVIEW_TEXT"]):?>
+<section class="article">
+  <header class="article__header">
+    <h1 class="article__title">
+    <?if($arParams["DISPLAY_NAME"]!="N" && $arResult["NAME"]):?>
+		  <?=$arResult["NAME"]?>
+	  <?endif;?>
+    </h1>
+    <?if($arParams["DISPLAY_DATE"]!="N" && $arResult["DISPLAY_ACTIVE_FROM"]):?>
+		<time class="article__publication-date"><?=$arResult["DISPLAY_ACTIVE_FROM"]?></time>
+	  <?endif;?>
+    <a class="back-link" href="/novosti/">
+      <svg class="icon" role="img">
+        <use xlink:href="<?=ASSET_PATH?>icons.svg#dropdown-arrow" /></svg>
+      Пресс-центр
+    </a>
+  </header>
+  <div class="article__content-wrapper">
+    <div class="article__lead content-block">
+      <?if($arParams["DISPLAY_PREVIEW_TEXT"]!="N" && $arResult["FIELDS"]["PREVIEW_TEXT"]):?>
+        <p><?=$arResult["FIELDS"]["PREVIEW_TEXT"];unset($arResult["FIELDS"]["PREVIEW_TEXT"]);?></p>
+      <?endif;?>
+      <?if($arParams["DISPLAY_PREVIEW_TEXT"]!="N" && $arResult["FIELDS"]["PREVIEW_TEXT"]):?>
 		<p><?=$arResult["FIELDS"]["PREVIEW_TEXT"];unset($arResult["FIELDS"]["PREVIEW_TEXT"]);?></p>
 	<?endif;?>
 	<?if($arResult["NAV_RESULT"]):?>
@@ -42,54 +46,60 @@ $this->setFrameMode(true);
 	<?else:?>
 		<?echo $arResult["PREVIEW_TEXT"];?>
 	<?endif?>
-	<div style="clear:both"></div>
-	<br />
-	<?foreach($arResult["FIELDS"] as $code=>$value):
-		if ('PREVIEW_PICTURE' == $code || 'DETAIL_PICTURE' == $code)
-		{
-			?><?=GetMessage("IBLOCK_FIELD_".$code)?>:&nbsp;<?
-			if (!empty($value) && is_array($value))
-			{
-				?><img border="0" src="<?=$value["SRC"]?>" width="<?=$value["WIDTH"]?>" height="<?=$value["HEIGHT"]?>"><?
-			}
-		}
-		else
-		{
-			?><?=GetMessage("IBLOCK_FIELD_".$code)?>:&nbsp;<?=$value;?><?
-		}
-		?><br />
-	<?endforeach;
-	foreach($arResult["DISPLAY_PROPERTIES"] as $pid=>$arProperty):?>
+    </div>
+  </div>
+  <div class="article__content-wrapper">
+    <div class="article__content content-block">
+    <?if($arParams["DISPLAY_PICTURE"]!="N" && is_array($arResult["DETAIL_PICTURE"])):?>
+      <img
+        class="detail_picture"
+        border="0"
+        src="<?=$arResult["DETAIL_PICTURE"]["SRC"]?>"
+        width="<?=$arResult["DETAIL_PICTURE"]["WIDTH"]?>"
+        height="<?=$arResult["DETAIL_PICTURE"]["HEIGHT"]?>"
+        alt="<?=$arResult["DETAIL_PICTURE"]["ALT"]?>"
+        title="<?=$arResult["DETAIL_PICTURE"]["TITLE"]?>"
+        />
+      <?endif?>
+      <div class="image-caption">
+        Подпись к&nbsp;фотографии. Как и&nbsp;сама фотография не&nbsp;является необходимой
+      </div>
+      <p>
+        <?if($arResult["NAV_RESULT"]):?>
+          <?if($arParams["DISPLAY_TOP_PAGER"]):?><?=$arResult["NAV_STRING"]?><br /><?endif;?>
+          <?echo $arResult["NAV_TEXT"];?>
+          <?if($arParams["DISPLAY_BOTTOM_PAGER"]):?><br /><?=$arResult["NAV_STRING"]?><?endif;?>
+        <?elseif(strlen($arResult["DETAIL_TEXT"])>0):?>
+          <?echo $arResult["DETAIL_TEXT"];?>
+        <?else:?>
+          <?echo $arResult["PREVIEW_TEXT"];?>
+        <?endif?>
+      </p>
+      <blockquote class="blockquote" data-controller="polar-lights-masked"
+        data-action="mousemove->polar-lights-masked#updateMaskPosition">
 
-		<?=$arProperty["NAME"]?>:&nbsp;
-		<?if(is_array($arProperty["DISPLAY_VALUE"])):?>
-			<?=implode("&nbsp;/&nbsp;", $arProperty["DISPLAY_VALUE"]);?>
-		<?else:?>
-			<?=$arProperty["DISPLAY_VALUE"];?>
-		<?endif?>
-		<br />
-	<?endforeach;
-	if(array_key_exists("USE_SHARE", $arParams) && $arParams["USE_SHARE"] == "Y")
-	{
-		?>
-		<div class="news-detail-share">
-			<noindex>
-			<?
-			$APPLICATION->IncludeComponent("bitrix:main.share", "", array(
-					"HANDLERS" => $arParams["SHARE_HANDLERS"],
-					"PAGE_URL" => $arResult["~DETAIL_PAGE_URL"],
-					"PAGE_TITLE" => $arResult["~NAME"],
-					"SHORTEN_URL_LOGIN" => $arParams["SHARE_SHORTEN_URL_LOGIN"],
-					"SHORTEN_URL_KEY" => $arParams["SHARE_SHORTEN_URL_KEY"],
-					"HIDE" => $arParams["SHARE_HIDE"],
-				),
-				$component,
-				array("HIDE_ICONS" => "Y")
-			);
-			?>
-			</noindex>
-		</div>
-		<?
-	}
-	?>
+        Один паром у&nbsp;нас сегодня все-таки перевёз несколько автомашин.
+        На&nbsp;11&nbsp;часов утра ветер на&nbsp;реке в&nbsp;районе переправы
+        Салехард-Лабытнанги усилился до&nbsp;25&nbsp;метров в&nbsp;секунду. Паромы
+        остановились. Прекращены также все перевозки водным транспортом. Из-за сильного
+        северо-восточного ветра из&nbsp;аэропорта Салехард временно прекращён
+        приём-выпуск вертолётных рейсов. Самолёты летают в&nbsp;штатном режиме
+        <div class="person-info">
+          <img class="person-info__photo" src="<?=ASSET_PATH?>stub/12.jpg" alt="Виталий Лагутин">
+          <div class="person-info__description">
+            <span class="person-info__name">Виталий Лагутин</span>
+            <span class="person-info__position">директор дирекции транспорта ЯНАО</span>
+          </div>
+        </div>
+        <div class="polar-lights polar-lights--dim">
+          <div class="polar-lights__mask" data-target="polar-lights-masked.mask"></div>
+        </div>
+      </blockquote>
+    </div>
+  </div>
+
+	<div style="clear:both"></div>
+	
+  
+</section>
 </div>
